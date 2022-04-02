e741def2f9e610230202990929c468875bcaff21
Gursharan Singh <gursharan@vsrkcapital.com>
2022-04-02 17:54:45 +0530

    bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 require('./db/mongoose')
-
+const userRoute = require('./routers/users')
+const taskRoute =require('./routers/tasks')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+////// Routers
+// user route
+app.use(userRoute)
+
+// task route
+app.use(taskRoute)
+
 
 app.get('',(req,res)=>{
     res.send({
@@ -12,84 +21,29 @@ app.get('',(req,res)=>{
     })
 })
 
-//Users
-const User = require('./models/users')
-
-app.post('/users', async (req,res)=>{
-    const user = new User(req.body)
-    try{
-       const u = await user.save()
-       res.status(201).send(u)
-    }catch(e){
-        res.status(406).send(e)
-    }
-})
-
-app.get('/users', async (req,res)=>{
-    try{
-       const users = await User.find({})
-        res.status(202).send(users)
-    }catch(e){
-        res.status(503).send(e)
-    }
-})
-
-app.get('/user/:id', async (req,res)=>{
-    const _id = req.params.id
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.status(202).send(user)
-    }catch(e){
-        res.status(503).send(e)
-    }
-
-})
 
 
-//tasks
-const Tasks = require('./models/tasks')
-
-app.post('/tasks', async (req,res)=>{
-    const task = new Tasks(req.body)
-    try{
-       const t = await task.save()
-       res.status(201).send(t)
-    }catch(e){
-        res.status(406).send(e)
-    }
-})
-
-
-app.get('/tasks', async (req,res)=>{
-    
-    try{
-        const tasks = await Tasks.find({})
-        res.status(202).send(tasks)
-    }catch(e){
-        res.status(503).send(e)
-    }
-
-})
-
-app.get('/task/:id', async (req,res)=>{
-    const _id = req.params.id
-
-    try{
-        const task = await Tasks.findById(_id)
-        if(!task){
-            return res.status(404).send()
-        }
-        res.status(202).send(task)
-    }catch(e){
-        res.status(503).send(e)
-    }
-})
 
 app.listen(port, ()=>{
     console.log("Server is up at port: "+port)
 })
 
 
+////testing encryption
+const bcrypt = require('bcryptjs')
+
+const tBcrypt =  ()=>{
+    const hashed = bcrypt.hash('test',8)
+    hashed.then((r)=>{
+        console.log('hashN',r)
+    })
+    
+} 
+const  tBcryptAsync = async ()=>{
+    const hashed = await bcrypt.hash('test',8)
+        console.log(hashed)
+        console.log(await bcrypt.compare('t4est',hashed))
+} 
+
+tBcrypt()
+tBcryptAsync()
