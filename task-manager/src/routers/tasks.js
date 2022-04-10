d6403afc0d483cee2bd1c5f2adf00a957965bf4e
gurivsrk <gursharan@vsrkcapital.com>
2022-04-11 01:18:24 +0530

    sorrting added

--- task-manager/src/routers/tasks.js
+++ task-manager/src/routers/tasks.js
@@ -19,17 +19,30 @@ route.post('/tasks', auth ,async (req,res)=>{
 })
 
 //// GET /task?completed=true
+//// GEt /task?limit=2&skip=2
+///// GET /task?sortBy=createdAt:desc
 route.get('/tasks',auth,async (req,res)=>{
-        const match = {}
+        const match = {}, sort = {}
         if(req.query.completed){
             match.completed = req.query.completed === 'true'
         }
+
+        if(req.query.sortBy){
+            const parts = req.query.sortBy.split(':')
+            sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+        }
+        
     try{
         //const tasks = await Tasks.find({author:req.user._id})
         //                 or
         const tasks = await req.user.populate({
             path:'tasks',
-            match
+            match,
+            options:{
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         })
         res.status(202).send(tasks.tasks)
     }catch(e){
